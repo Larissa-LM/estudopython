7b801c9aa556eeb1b9f20298e67a5c902ca1b8e8
Larissa-LM <larissa.maria@creditas.com>
2022-04-20 09:28:36 -0300

    Aprimorando o código advinhaçao

--- python-3-parte-1/advinhacao.py
+++ python-3-parte-1/advinhacao.py
@@ -3,13 +3,26 @@ print("Bem-vindo ao jogo de advinhação")
 print("*******************************")
 
 numero_secreto = 42
- 
-chute = input("Digite o seu número: ") # Aqui recebe uma string "42" precisa converter
-numero_chutado = int(chute) #conversão 
+total_de_tentativas = 3
+rodada = 1
 
-print("Você digitou" , chute)
+while(rodada <= total_de_tentativas ): 
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = input("Digite o seu número: ") # Aqui recebe uma string "42" precisa converter
+    numero_chutado = int(chute) #conversão
 
-if(numero_chutado == numero_secreto):
-    print("Você acertou!!")
-else:
-    print("Você errou!!")
+    print("Você digitou" , chute)
+
+    acertou = numero_chutado == numero_secreto
+
+    if(acertou):
+        print("Você acertou!!")
+    else:
+        if(numero_chutado > numero_secreto):
+            print("Você errou!! O seu chute foi maior que o número secreto")
+        elif(numero_chutado < numero_secreto):
+            print("Você errou!! O seu chute foi menor que o número secreto")
+
+    rodada = rodada + 1 
+
+    print("Fim do jogo")
