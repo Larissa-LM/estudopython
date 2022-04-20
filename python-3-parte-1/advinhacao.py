5b5d436bb064fcdf22cbc4c0ebfa1bd5bcd06a7d
Larissa-LM <larissa.maria@creditas.com>
2022-04-20 14:38:11 -0300

    Adicinando aviso sobre qual era o num secreto e pontuação final

--- python-3-parte-1/advinhacao.py
+++ python-3-parte-1/advinhacao.py
@@ -5,7 +5,7 @@ print("Bem-vindo ao jogo de advinhação")
 print("*******************************")
 
 numero_secreto = random.randrange(1,101)
-print(numero_secreto)
+#print(numero_secreto)
 total_de_tentativas = 3
 rodada = 1
 pontos = 1000
@@ -13,7 +13,7 @@ pontos = 1000
 print("Qual nível de dificuldade? \n")
 print("(1) Fácil (2) Médio (3) Difícil \n")
 
-nivel = int(input("Defina o nível:"))
+nivel = int(input("Defina o nível: "))
 
 if(nivel == 1):
     total_de_tentativas = 5
@@ -27,22 +27,28 @@ for rodada in range(1,total_de_tentativas + 1):
     chute = input("Digite um número entre 1 e 100: ") # Aqui recebe uma string "42" precisa converter
     numero_chutado = int(chute) #conversão
 
-    print("Você digitou" , chute)
+    print("Você digitou\n" , chute)
 
     if(numero_chutado < 1 or numero_chutado > 100):
         print("Você deve digitar um número entre 1 e 100 \n")
         continue
 
     acertou = numero_chutado == numero_secreto
+    maior = numero_chutado > numero_secreto
+    menor = numero_chutado < numero_secreto
 
     if(acertou):
-        print("Você acertou!!Total de pontos {}".format(pontos))
+        print("Você acertou!!Total de pontos {}\n".format(pontos))
         break
     else:
-        if(numero_chutado > numero_secreto):
-            print("Você errou!! O seu chute foi maior que o número secreto")
-        elif(numero_chutado < numero_secreto):
-            print("Você errou!! O seu chute foi menor que o número secreto")
+        if(maior):
+            print("Você errou!! O seu chute foi maior que o número secreto\n")
+            if(rodada == total_de_tentativas):
+                print("O número secreto era {}. Você fez {}\n".format(numero_secreto, pontos))
+        elif(menor):
+            print("Você errou!! O seu chute foi menor que o número secreto\n")
+            if(rodada == total_de_tentativas):
+                print("O número secreto era {}. Você fez {}\n".format(numero_secreto, pontos))
         pontos_perdidos = abs(numero_secreto - numero_chutado) # 20 - 10 = 10 abs() deixa o resultado positivo
         pontos = pontos - pontos_perdidos 
 
