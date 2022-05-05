490c0011facaf063468f68aaf0c896ed2a0f2685
Larissa-LM <larissa.maria@creditas.com>
2022-05-05 16:13:56 -0300

    Aprendendo sobre herança

--- python-oo-parte1-parte2/modelo_parte2.py
+++ python-oo-parte1-parte2/modelo_parte2.py
@@ -1,66 +1,53 @@
-from select import select
+class Programa: 
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano 
+        self._likes = 0 
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self,novo_nome):
+        self._nome = novo_nome.title()
+
+    @property
+    def likes(self): 
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
 
 
-class Filme: 
+class Filme(Programa): #Herdando da classe Programa
 
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano 
         self.duracao = duracao
-        self.__likes = 0 
-
-    @property
-    def nome(self):
-        return self.nome
-
-    @nome.setter
-    def nome(self,novo_nome):
-        self.nome = novo_nome.title()
-
-    @property
-    def likes(self): 
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
-
-    def dar_like(self):
-        self.likes += 1
+        self._likes = 0 
 
 
-class Serie: 
+class Serie(Programa): 
 
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano 
         self.temporadas = temporadas
-        self.__likes = 0 
+        self._likes = 0 
     
-    @property
-    def nome(self):
-        return self.nome
-
-    @nome.setter
-    def nome(self,novo_nome):
-        self.nome = novo_nome.title()
-
-    @property
-    def likes(self): 
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
 
 
 vingadores = Filme("Vingadores - Guerra infinita", 2018, 160)
 vingadores.dar_like()
 
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes:{vingadores.like} ')
+print(f'Nome: {vingadores._nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes:{vingadores._likes} ')
 
 mordern_family = Serie("modern family", 2009, 11)
 mordern_family.dar_like()
 
-print(f'Nome: {mordern_family.nome} - Ano: {mordern_family.ano} - Temporadas: {mordern_family.temporadas} - Likes:{mordern_family.like}')
+print(f'Nome: {mordern_family._nome} - Ano: {mordern_family.ano} - Temporadas: {mordern_family.temporadas} - Likes:{mordern_family._likes}')
 
 
     
