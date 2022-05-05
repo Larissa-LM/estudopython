1865c2a7fb8618503daa2c50904a843af2b5962a
Larissa-LM <larissa.maria@creditas.com>
2022-05-05 10:50:02 -0300

    Revendo criação de métodos e atributos na parte 2 do curso

--- python-oo-parte1-parte2/modelo_parte2.py
+++ python-oo-parte1-parte2/modelo_parte2.py
@@ -1,24 +1,66 @@
+from select import select
+
+
 class Filme: 
 
     def __init__(self, nome, ano, duracao):
-        self.nome = nome 
+        self.__nome = nome.title()
         self.ano = ano 
         self.duracao = duracao
+        self.__likes = 0 
+
+    @property
+    def nome(self):
+        return self.nome
+
+    @nome.setter
+    def nome(self,novo_nome):
+        self.nome = novo_nome.title()
+
+    @property
+    def likes(self): 
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
+
+    def dar_like(self):
+        self.likes += 1
 
 
 class Serie: 
 
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano 
         self.temporadas = temporadas
+        self.__likes = 0 
+    
+    @property
+    def nome(self):
+        return self.nome
+
+    @nome.setter
+    def nome(self,novo_nome):
+        self.nome = novo_nome.title()
+
+    @property
+    def likes(self): 
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
 
 
 vingadores = Filme("Vingadores - Guerra infinita", 2018, 160)
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}')
+vingadores.dar_like()
 
-mordern_family = Serie("Modern Family", 2009, 11)
-print(f'Nome: {mordern_family.nome} - Ano: {mordern_family.ano} - Temporadas: {mordern_family.temporadas}')
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes:{vingadores.like} ')
+
+mordern_family = Serie("modern family", 2009, 11)
+mordern_family.dar_like()
+
+print(f'Nome: {mordern_family.nome} - Ano: {mordern_family.ano} - Temporadas: {mordern_family.temporadas} - Likes:{mordern_family.like}')
 
 
     
