9bfaf7a7d3ae911b6c804a8c83c14f583e98895c
Larissa-LM <larissa.maria@creditas.com>
2022-05-06 11:17:14 -0300

    Aprendendo sobre polimorfismo

--- python-oo-parte1-parte2/modelo_parte2.py
+++ python-oo-parte1-parte2/modelo_parte2.py
@@ -23,31 +23,38 @@ class Programa:
 class Filme(Programa): #Herdando da classe Programa
 
     def __init__(self, nome, ano, duracao):
-        self._nome = nome.title()
-        self.ano = ano 
+        #self._nome = nome.title()
+        #self.ano = ano 
+        #self._likes = 0 Para evitar repetição de código, existe outra maneira de usar atributos da classe mãe:
+
+        super().__init__(nome,ano)
         self.duracao = duracao
-        self._likes = 0 
 
 
 class Serie(Programa): 
 
     def __init__(self, nome, ano, temporadas):
-        self._nome = nome.title()
-        self.ano = ano 
+        super().__init__(nome,ano)
         self.temporadas = temporadas
-        self._likes = 0 
+        
     
 
 
 vingadores = Filme("Vingadores - Guerra infinita", 2018, 160)
 vingadores.dar_like()
 
-print(f'Nome: {vingadores._nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes:{vingadores._likes} ')
+print(f'Nome: {vingadores._nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes:{vingadores.likes} ')
 
 mordern_family = Serie("modern family", 2009, 11)
 mordern_family.dar_like()
 
-print(f'Nome: {mordern_family._nome} - Ano: {mordern_family.ano} - Temporadas: {mordern_family.temporadas} - Likes:{mordern_family._likes}')
+print(f'Nome: {mordern_family._nome} - Ano: {mordern_family.ano} - Temporadas: {mordern_family.temporadas} - Likes:{mordern_family.likes}')
+
+filmes_e_series = [vingadores,mordern_family]
+
+for programa in filmes_e_series: 
+    detalhes = programa.duracao if hasattr(programa, "duracao") else programa.temporadas # hasattr é utilizado para verificar se um atributo existe em uma classe (existe duracaqo dentro do objeto programa?)
+    print(f'{programa.nome} - {detalhes}  - {programa.likes}')
 
 
     
