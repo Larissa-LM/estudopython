e991a5508d40c3c75cae5bdd59f06028b59b9081
Larissa-LM <larissa.maria@creditas.com>
2022-05-06 12:02:15 -0300

    Reduzindo If

--- python-oo-parte1-parte2/modelo_parte2.py
+++ python-oo-parte1-parte2/modelo_parte2.py
@@ -2,7 +2,7 @@ class Programa:
     def __init__(self, nome, ano):
         self._nome = nome.title()
         self.ano = ano 
-        self._likes = 0 
+        self._likes = 0 #atributo protegido 
 
     @property
     def nome(self):
@@ -18,7 +18,9 @@ class Programa:
 
     def dar_like(self):
         self._likes += 1
-
+    
+    def imprime(self): 
+        print(f'{self._nome} - {self.ano} - {self._likes} likes')
 
 class Filme(Programa): #Herdando da classe Programa
 
@@ -29,32 +31,34 @@ class Filme(Programa): #Herdando da classe Programa
 
         super().__init__(nome,ano)
         self.duracao = duracao
-
+    
+    def imprime(self): 
+        print(f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} likes')
 
 class Serie(Programa): 
 
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome,ano)
         self.temporadas = temporadas
-        
+    
+    def imprime(self): 
+        print(f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} likes')
+
     
 
 
 vingadores = Filme("Vingadores - Guerra infinita", 2018, 160)
 vingadores.dar_like()
 
-print(f'Nome: {vingadores._nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes:{vingadores.likes} ')
-
 mordern_family = Serie("modern family", 2009, 11)
 mordern_family.dar_like()
-
-print(f'Nome: {mordern_family._nome} - Ano: {mordern_family.ano} - Temporadas: {mordern_family.temporadas} - Likes:{mordern_family.likes}')
 
 filmes_e_series = [vingadores,mordern_family]
 
 for programa in filmes_e_series: 
-    detalhes = programa.duracao if hasattr(programa, "duracao") else programa.temporadas # hasattr é utilizado para verificar se um atributo existe em uma classe (existe duracaqo dentro do objeto programa?)
-    print(f'{programa.nome} - {detalhes}  - {programa.likes}')
+    #detalhes = programa.duracao if hasattr(programa, "duracao") else programa.temporadas # hasattr é utilizado para verificar se um atributo existe em uma classe (existe duracaqo dentro do objeto programa?)
+    #Reduzindo if
+    programa.imprime()
 
 
     
