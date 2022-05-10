92b2845d9522ea570182e7e167538e3027c46306
Larissa-LM <larissa.maria@creditas.com>
2022-05-10 10:14:31 -0300

    Aprendendo MRO e Mixin

--- python-oo-parte1-parte2/modelo2_parte2.py
+++ python-oo-parte1-parte2/modelo2_parte2.py
@@ -1,4 +1,7 @@
 class Funcionario:
+    def __init__(self,nome):
+        self.nome = nome
+
     def registra_horas(self, horas):
         print('Horas registradas.')
 
@@ -19,19 +22,26 @@ class Alura(Funcionario):
     def busca_perguntas_sem_resposta(self):
         print('Mostrando perguntas não respondidas do fórum')
 
+class Hipster(Funcionario): 
+    def __str__(self):
+        return f'Hipster, {self.nome}'
+
 class Junior(Alura):
     pass
 
 class Pleno(Alura, Caelum):
     pass
 
-class Senior(Alura, Caelum):
+class Senior(Alura, Caelum,Hipster):
     pass 
 
 
-luna = Junior()
-marie = Pleno()
+luna = Junior("Luna")
+marie = Pleno("Marie")
 
 luna.busca_perguntas_sem_resposta()
 
 marie.busca_cursos_do_mes()
+
+berfin = Senior("Berfin")
+print(berfin)
