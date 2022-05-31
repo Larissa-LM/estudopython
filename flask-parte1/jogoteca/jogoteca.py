a61371e9cde47a6b326a1c490e752b9e18961262
Larissa-LM <larissa.maria@creditas.com>
2022-05-31 16:04:02 -0300

    Estilizando o form de novo jogo e editar ao adicionar uma imagem

--- flask-parte1/jogoteca/jogoteca.py
+++ flask-parte1/jogoteca/jogoteca.py
@@ -1,6 +1,6 @@
 
 from distutils.log import debug
-from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask import Flask, render_template, request, redirect, session, flash, url_for, send_from_directory
 
 from flask_mysqldb import MySQL 
 from dao import JogoDao, UsuarioDao
@@ -25,7 +25,7 @@ jogo_dao = JogoDao(db)
 usuario_dao = UsuarioDao(db)
 
 
- @app.route('/') #para colocar uma informação no site precisamos colocar uma rota e em seguida precisamos criar uma função para definir o que existe dentro dessa rota
+@app.route('/') #para colocar uma informação no site precisamos colocar uma rota e em seguida precisamos criar uma função para definir o que existe dentro dessa rota
 def index():
     lista_de_jogos = jogo_dao.listar()
     return render_template("lista.html", titulo = "Jogos", jogos = lista_de_jogos) # Não é uma boa prática utilizar o html direto e sim importá-lo. render_tempĺate sabe que existe uma pasta templates então é só passar o arquivo
@@ -48,10 +48,9 @@ def criar_novo_jogo():
     console = request.form["console"]
     jogo = Jogo(nome,categoria,console)
     jogo = jogo_dao.salvar(jogo)
-    imagem = request.files["imagem"]
-    imagem.save(f'{}/{imagem.filename}')
+    imagem = request.files["arquivo"]
     upload_path = app.config['UPLOAD_PATH']
-    imagem.save(f'{upload_path}/capa{jogo.id},jpg') # salvando o nome do arquivo com o id do jogo
+    imagem.save(f'{upload_path}/capa{jogo.id}.jpg') # salvando o nome do arquivo com o id do jogo
     #imagem.save(f'uploads/{imagem.filename}') Caminho para a imagem chumbado
     
     return redirect(url_for("index")) #redirecionando para a página inicial
@@ -61,7 +60,7 @@ def editar(id):
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('editar', id = id)))
     jogo = jogo_dao.busca_por_id(id)
-    return render_template('editar.html', titulo = 'Editando jogo', jogo = jogo)
+    return render_template('editar.html', titulo = 'Editando jogo', jogo = jogo, capa_jogo = f'capa{id}.jpg')
 
 
 @app.route("/atualizar", methods = ["POST" ,]) #Rota intermediária
@@ -109,4 +108,11 @@ def logout():
     flash("Logout realizado com sucesso!")
     return redirect(url_for("index"))
 
+@app.route("/uploads/<nome_arquivo>")
+def imagem(nome_arquivo): 
+    return send_from_directory('uploads', nome_arquivo)
+
+
+
 app.run(debug=True) #para conseguir rodar a aplicação 
+
